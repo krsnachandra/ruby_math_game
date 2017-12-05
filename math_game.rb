cd9f8dacb027b6aead4ae0c07103f3e1644bddcd
Chandra <krsna.c@gmail.com>
2017-12-05 00:54:56 +0000

    added gitignore

--- math_game.rb
+++ math_game.rb
@@ -1,8 +1,18 @@
+require './player'
+
 class MathGame
 
-def self.main
+  def self.main
     game = MathGame.new
     game.start
+  end
+
+  def initialize
+    @players = [
+      Player.new('Player 1'),
+      Player.new('Player 2')
+    ]
+    @current_player = 0
   end
 
 end
---
+++ player.rb
@@ -0,0 +1,17 @@
+class Player
+
+  attr_accessor :name
+
+  attr_accessor :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+end
+
